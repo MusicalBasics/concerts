c0d2dc3d891d25e47093da72987d678d49f6838f
Thomas Zhang <thomas@mier.cat>
2023-10-24 16:59:37 -0700

    feat: added meta data

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import { createTheme } from "@mui/material";
 import { Analytics } from "@vercel/analytics/react";
 import "mapbox-gl/dist/mapbox-gl.css";
 import "../styles/globals.css";
+import Head from "next/head";
 
 const theme = createTheme({
   palette: {
@@ -22,6 +23,15 @@ export default function MyApp({ Component, pageProps }) {
 
   return getLayout(
     <ThemeProvider theme={theme}>
+      <Head>
+        <title>MusicalBasics Concerts</title>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <meta
+          name="description"
+          content="MusicalBasics / We Are One concerts and events app."
+        />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Component {...pageProps} />
       <Analytics />
     </ThemeProvider>
