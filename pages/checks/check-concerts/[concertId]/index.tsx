cd4f8aa4bc6e393501f039d5a65a630560ec856b
Thomas Zhang <thomas@mier.cat>
2023-11-02 02:56:30 -0700

    feat: checkers

---
+++ pages/checks/check-concerts/[concertId]/index.tsx
@@ -0,0 +1,167 @@
+// import SeatingMap from "@/components/concerts/SeatingMaps";
+import React, { FC, useState } from "react";
+import dynamic from "next/dynamic";
+import { sanityClient } from "@/utils/sanity";
+import { GetServerSideProps } from "next";
+import { Section } from "@/models/Section";
+import { Container, Stack, Typography } from "@mui/material";
+import _ from "lodash";
+import { Concert } from "@/models/Concert";
+import { getFormattedDate } from "@/utils/concert-utils";
+
+const SeatingMap = dynamic(
+  () => import("@/components/seating-map/seating-map"),
+  {
+    ssr: false,
+  }
+);
+
+interface CanvasPageProps {
+  // Define any props needed for the component
+  sections: Section[];
+  concert: Concert;
+}
+
+const CanvasPage: FC<CanvasPageProps> = ({ sections, concert }) => {
+  // Initialize state if needed
+  const [state, setState] = useState<CanvasPageState>({
+    // ...
+  });
+
+  // Define any necessary functions for the component
+  const myFunction = (): void => {
+    // ...
+  };
+
+  // Is reserved but not reserved by any user (no customer reference)
+  const seatsReservedWithoutUser = _.flatten(
+    sections.map((section) =>
+      section.rows.map((row) =>
+        row.seats.filter((seat) => seat.isReserved && !seat.reservedBy)
+      )
+    )
+  );
+
+  return (
+    // Define the component's JSX
+    <Container>
+      <Stack
+        py={5}
+        sx={{
+          color: "white",
+        }}
+        gap={2}
+      >
+        <Typography variant="h2">{concert.name}</Typography>
+        <Stack direction="row" gap={5} mb={5}>
+          <Typography variant="h5">{getFormattedDate(concert.date)}</Typography>
+          <Typography variant="h5">{concert.venue.name}</Typography>
+        </Stack>
+        <Typography variant="h4">Seating Map</Typography>
+      </Stack>
+      <SeatingMap sections={sections} curve={0.0005} />
+      <Stack sx={{ color: "wheat" }} mt={5}>
+        <Typography variant="h4">Seats Reserved Without User</Typography>
+        {seatsReservedWithoutUser.map((seats) => {
+          return (
+            <Stack>
+              {seats.length === 0
+                ? "Empty"
+                : seats.map((seat) => {
+                    return <p>{seat.number}</p>;
+                  })}
+            </Stack>
+          );
+        })}
+      </Stack>
+    </Container>
+  );
+};
+
+interface CanvasPageState {
+  // Define any state needed for the component
+}
+
+export default CanvasPage;
+
+// Data Fetching
+export const getServerSideProps = (async (context) => {
+  const { concertId } = context.params;
+
+  const concerts = await sanityClient.fetch(
+    `*[_type == "concert" && _id == $concertId]{
+      _id,
+      name,
+      date,
+      venue-> {
+        _id,
+        name,
+        city-> {
+          _id,
+          name,
+        },
+      },
+      seatingChart-> {
+        _id,
+        name,
+      },
+    }
+  `,
+    { concertId }
+  );
+
+  if (!concerts.length) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const concert = concerts[0];
+  const { seatingChart } = concert;
+
+  console.log("seatingChart", seatingChart);
+  if (!seatingChart) {
+    return {
+      notFound: true,
+    };
+  }
+  const seatingChartId = seatingChart._id;
+
+  const seatingCharts = await sanityClient.fetch(
+    `*[_type == "seatingChart" && _id == $seatingChartId]{
+      sections[] {
+        name,
+        rows[] {
+          id,
+          seats[] {
+            number,
+            isReserved,
+            isReservable,
+            reservedBy-> {
+              _id,
+              name,
+              email,
+            },
+          },
+        },
+      },
+    }
+  `,
+    { seatingChartId }
+  );
+
+  if (!seatingCharts || seatingCharts.length === 0) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const { sections } = seatingCharts[0];
+
+  return {
+    props: {
+      concert,
+      sections, // now concert includes dereferenced city and venue
+    },
+  };
+}) satisfies GetServerSideProps;
---
+++ pages/checks/check-concerts/index.tsx
@@ -0,0 +1,105 @@
+// import SeatingMap from "@/components/concerts/SeatingMaps";
+import React, { FC, useState } from "react";
+import dynamic from "next/dynamic";
+import { sanityClient } from "@/utils/sanity";
+import { GetServerSideProps } from "next";
+import { Section } from "@/models/Section";
+import { Button, Container, Grid, Stack } from "@mui/material";
+import Link from "next/link";
+import _ from "lodash";
+
+interface CheckConcertsPageProps {
+  // Define any props needed for the component
+  sections: Section[];
+}
+
+const CheckConcertsPage: FC<CheckConcertsPageProps> = ({ concerts }) => {
+  // Initialize state if needed
+
+  // Define any necessary functions for the component
+
+  return (
+    // Define the component's JSX
+    <Container>
+      <Grid container spacing={2}>
+        {concerts.map((concert) => (
+          <Grid item xs={12} md={6} lg={4} key={concert._id}>
+            <Stack
+              sx={{
+                color: "white",
+              }}
+              py={3}
+            >
+              <h1>{concert.name}</h1>
+              <p>{concert.date}</p>
+              <p>{concert.city.name}</p>
+              <p>{concert.venue?.name || "No Venue"}</p>
+            </Stack>
+            <Link href={`/secrets/check-concerts/${concert._id}`}>
+              <Button
+                variant="contained"
+                sx={{
+                  backgroundColor: "#000000",
+                  color: "white",
+                }}
+              >
+                Check Concert
+              </Button>
+            </Link>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
+};
+
+interface CanvasPageState {
+  // Define any state needed for the component
+}
+
+export default CheckConcertsPage;
+
+// Data Fetching
+export const getServerSideProps = (async (context) => {
+  // const TEST_CONCERT_ID = "4f4b9063-70e7-457f-b4b4-8494eadb85c1";
+  // const seatingChartId = "db1714bf-361a-4bf9-88a6-6950949dc532";
+
+  const concerts = await sanityClient.fetch(
+    `*[_type == "concert"]{
+      _id,
+      name,
+      date,
+      city-> {
+        _id,
+        name,
+      },
+      venue-> {
+        _id,
+        name,
+        city-> {
+          _id,
+          name,
+        },
+      },
+      seatingChart-> {
+        _id,
+        name,
+      },
+    }
+  `
+  );
+
+  if (!concerts.length) {
+    return {
+      props: {
+        sections: [],
+      },
+    };
+  }
+
+  return {
+    props: {
+      concerts: _.sortBy(concerts, ["date"], ["asc"]),
+    },
+  };
+}) satisfies GetServerSideProps;
