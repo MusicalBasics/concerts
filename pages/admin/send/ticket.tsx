f56c6a6cc499a837c4fafd9474801482b1372985
Thomas Zhang <thomas@mier.cat>
2023-11-06 21:27:20 -0800

    feat: send ticket(s)

--- pages/admin/send/ticket.tsx
+++ pages/admin/send/ticket.tsx
@@ -1,30 +1,18 @@
-import { sanityClient } from "@/utils/sanity";
-import { GetServerSideProps, GetStaticProps } from "next";
 import React, { FC } from "react";
-import _, { orderBy } from "lodash";
+import _ from "lodash";
 import { useState } from "react";
 import {
   Button,
   Container,
-  Input,
-  List,
-  ListItem,
   Stack,
   TextField,
-  Typography,
 } from "@mui/material";
 import { isInvalidEmail, isInvalidateTicket } from "@/utils/ticket-utils";
 import axios from "axios";
 
-const CheckTicketsPage: FC<CheckTicketsPageProps> = ({ duplicateTickets }) => {
-  const [copiedId, setCopiedId] = useState(null);
+const SendTicketPage: FC<SendTicketPageProps> = () => {
   const [enteredEmail, setEnteredEmail] = useState("");
   const [ticketNumber, setTicketNumber] = useState("");
-
-  const copyToClipboard = (id: any) => {
-    navigator.clipboard.writeText(id);
-    setCopiedId(id);
-  };
 
   const handleDownload = () => {
     try {
@@ -146,81 +134,10 @@ const CheckTicketsPage: FC<CheckTicketsPageProps> = ({ duplicateTickets }) => {
           Send Freaking Ticket!
         </Button>
       </Stack>
-      <Typography variant="h4" gutterBottom>
-        Check Tickets
-      </Typography>
-
-      <List>
-        {duplicateTickets.length === 0 && (
-          <ListItem>
-            <Typography variant="h4">No duplicate tickets found!</Typography>
-          </ListItem>
-        )}
-        {duplicateTickets.map((ticket) => (
-          <ListItem key={ticket.number}>
-            <Typography variant="h4">{ticket.number}: </Typography>
-            <Stack>
-              {ticket.ids.map((id) => (
-                <Stack
-                  direction={"row"}
-                  spacing={2}
-                  key={id}
-                  p={1}
-                  alignItems={"center"}
-                >
-                  <Typography variant="body1">{id}</Typography>
-                  <Button
-                    variant="outlined"
-                    size="small"
-                    color="secondary"
-                    onClick={() => copyToClipboard(id)}
-                  >
-                    {copiedId === id ? "Copied!" : "Copy"}
-                  </Button>
-                </Stack>
-              ))}
-            </Stack>
-          </ListItem>
-        ))}
-      </List>
     </Container>
   );
 };
 
-export default CheckTicketsPage;
+export default SendTicketPage;
 
-export const getServerSideProps = (async () => {
-  const duplicateTickets = await findDuplicateTickets();
-
-  return {
-    props: { duplicateTickets },
-  };
-}) satisfies GetServerSideProps;
-
-async function findDuplicateTickets() {
-  try {
-    const query = '*[_type == "ticket"]{number, _id}';
-    const tickets = await sanityClient.fetch(query);
-    const groupedTickets = _.groupBy(tickets, "number");
-    const duplicates: DuplicateTicket[] = [];
-    _.forEach(groupedTickets, (ticketGroup, number) => {
-      if (ticketGroup.length > 1) {
-        const ids = ticketGroup.map((ticket) => ticket._id);
-        duplicates.push({ number, ids });
-      }
-    });
-
-    return _.orderBy(duplicates, "number");
-  } catch (error: any) {
-    console.error("Error fetching tickets:", error.message);
-  }
-}
-
-// Type Definitions
-interface DuplicateTicket {
-  number: string;
-  ids: string[];
-}
-interface CheckTicketsPageProps {
-  duplicateTickets: DuplicateTicket[];
-}
+interface SendTicketPageProps {}
