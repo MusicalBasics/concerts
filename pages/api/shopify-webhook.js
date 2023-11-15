b7e0f12c38fa938792b5736d08aa99d7263d26fe
Lionel Yu <support@musicalbasics.com>
2023-11-14 19:44:07 -0800

    feat: final shopify webhook update

--- pages/api/shopify-webhook.js
+++ pages/api/shopify-webhook.js
@@ -1,4 +1,13 @@
 import crypto from 'crypto';
+import { createClient } from '@sanity/client';
+import { v4 as uuidv4 } from 'uuid';
+
+const client = createClient({
+  apiVersion: '2022-03-25',
+  projectId: 'zqcyefig',
+  dataset: 'production',
+  token: 'skcEET1oF0tK6FltENUhc87cmaJ3DgMngWVFCbO6UgLz8VieuUI4s817z3vuROMGwuPngGI9GkicV4xqZssf0wRWFRjKsWOHEv4eJzPCPScsncs1Lsnt3KdLTtjQSLfKvSp5fEaOwCthl1l6tAR02VHXRU4fJlG2gCosIsbESvwWxUn357ob',
+});
 
 export const config = {
   api: {
@@ -14,30 +23,37 @@ export default function handler(req, res) {
       rawData += chunk;
     });
 
-    req.on('end', () => {
+    req.on('end', async () => {
       console.log('Headers:', req.headers);
       console.log('Raw Body:', rawData);
 
-      // Your Shopify secret used for HMAC verification
       const SHOPIFY_SECRET = '37ccffd436511de0f2b580de1d6a70672b0d6c3d8f82cd9ca9ea00fa90e18db5';
 
-      // Extract the HMAC from the headers
       const hmacHeader = req.headers['x-shopify-hmac-sha256'];
       const generatedHash = crypto
         .createHmac('sha256', SHOPIFY_SECRET)
         .update(rawData, 'utf8', 'hex')
         .digest('base64');
 
-      // Log the HMAC values for comparison
       console.log('Received HMAC:', hmacHeader);
       console.log('Generated HMAC:', generatedHash);
 
       if (generatedHash === hmacHeader) {
         console.log('Webhook verified and received:', rawData);
+        
+        const data = JSON.parse(rawData);
+        const lineItems = data.line_items || [];
+        const ticketItem = lineItems.some(item => item.name.toLowerCase().includes("ticket"));
+        
+        if (ticketItem) {
+          const customerName = `${data.customer.first_name} ${data.customer.last_name}`;
+          const customerEmail = data.customer.email;
+          await createOrUpdateCustomer(customerName, customerEmail);
+        }
+
         res.status(200).json({ message: 'Webhook received and verified' });
       } else {
         console.log('Webhook verification failed');
-        console.log('Mismatch between received and generated HMAC');
         res.status(401).json({ message: 'Unauthorized - Webhook verification failed' });
       }
     });
@@ -46,3 +62,25 @@ export default function handler(req, res) {
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
+
+const createOrUpdateCustomer = async (customerName, customerEmail) => {
+  const cleanedEmail = customerEmail.trim();
+  const query = `*[_type == "customer" && email == "${cleanedEmail}"]`;
+  console.log('Running query:', query);
+  const existingCustomers = await client.fetch(query);
+  console.log('Query results:', existingCustomers);
+  if (existingCustomers.length > 0) {
+    return existingCustomers[0];
+  }
+
+  const newCustomer = {
+    _type: 'customer',
+    _key: uuidv4(),
+    name: customerName,
+    email: cleanedEmail,
+    tickets: []
+  };
+  const createdCustomer = await client.create(newCustomer);
+  console.log('New customer created:', createdCustomer);
+  return createdCustomer;
+};
