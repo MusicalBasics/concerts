b41e557f6ded712d7dcea2b7bc21cb47c1acda51
Lionel Yu <support@musicalbasics.com>
2023-11-14 19:04:20 -0800

    feat: shopify webhook

--- pages/api/shopify-webhook.js
+++ pages/api/shopify-webhook.js
@@ -1,37 +1,46 @@
-// pages/api/shopify-webhook.js
 import crypto from 'crypto';
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
 
 export default function handler(req, res) {
   if (req.method === 'POST') {
+    let rawData = '';
 
-    // Convert the request body to a string for logging
-    const bodyString = JSON.stringify(req.body);
+    req.on('data', chunk => {
+      rawData += chunk;
+    });
 
-    console.log('Headers:', req.headers);
-    console.log('Body:', bodyString);
+    req.on('end', () => {
+      console.log('Headers:', req.headers);
+      console.log('Raw Body:', rawData);
 
-    // Your Shopify secret used for HMAC verification
-    const SHOPIFY_SECRET = '37ccffd436511de0f2b580de1d6a70672b0d6c3d8f82cd9ca9ea00fa90e18db5';
+      // Your Shopify secret used for HMAC verification
+      const SHOPIFY_SECRET = '37ccffd436511de0f2b580de1d6a70672b0d6c3d8f82cd9ca9ea00fa90e18db5';
 
-    // Extract the HMAC from the headers
-    const hmacHeader = req.headers['x-shopify-hmac-sha256'];
-    const generatedHash = crypto
-      .createHmac('sha256', SHOPIFY_SECRET)
-      .update(bodyString, 'utf8', 'hex')
-      .digest('base64');
+      // Extract the HMAC from the headers
+      const hmacHeader = req.headers['x-shopify-hmac-sha256'];
+      const generatedHash = crypto
+        .createHmac('sha256', SHOPIFY_SECRET)
+        .update(rawData, 'utf8', 'hex')
+        .digest('base64');
 
-    // Log the HMAC values for comparison
-    console.log('Received HMAC:', hmacHeader);
-    console.log('Generated HMAC:', generatedHash);
+      // Log the HMAC values for comparison
+      console.log('Received HMAC:', hmacHeader);
+      console.log('Generated HMAC:', generatedHash);
 
-    if (generatedHash === hmacHeader) {
-      console.log('Webhook verified and received:', bodyString);
-      res.status(200).json({ message: 'Webhook received and verified' });
-    } else {
-      console.log('Webhook verification failed');
-      console.log('Mismatch between received and generated HMAC');
-      res.status(401).json({ message: 'Unauthorized - Webhook verification failed' });
-    }
+      if (generatedHash === hmacHeader) {
+        console.log('Webhook verified and received:', rawData);
+        res.status(200).json({ message: 'Webhook received and verified' });
+      } else {
+        console.log('Webhook verification failed');
+        console.log('Mismatch between received and generated HMAC');
+        res.status(401).json({ message: 'Unauthorized - Webhook verification failed' });
+      }
+    });
   } else {
     res.setHeader('Allow', ['POST']);
     res.status(405).end(`Method ${req.method} Not Allowed`);
